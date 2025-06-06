95c517d0a798e977539133547f429b579f0f0c5a
Giorgio Medico <giorgio.medico@studio.unibo.it>
2025-06-06 15:14:45 +0200

    exam

---
+++ NoLoad/exam/exam.py
@@ -0,0 +1,5 @@
+
+    Puoi usare qualsiasi architettura (encoder-decoder, decoder-only, ecc.).
+    Il numero massimo di parametri del modello non deve superare i 2 milioni.
+    La generazione della notazione postfissa deve essere autoregressiva (se usi un modello encoder-decoder).
+    Non è consentito utilizzare la beam search durante la generazione.
